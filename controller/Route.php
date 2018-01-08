51a1fc1214eec19d24650660de5ad9486591050f
Baptiste <baptistemoine@outlook.com>
2018-01-08 16:41:27 +0100

    Ajout Error Route
    add class and add view

--- controller/Route.php
+++ controller/Route.php
@@ -1,7 +1,41 @@
 <?php
-function __autoload($className){
-    require_once 'classes/'.$className.'.php';
-}
-class Routeur {
+require_once 'controller/ControllerContact.php';
+require_once 'controller/ControllerAccueil.php';
+require_once '/view/View.php';
+class Route {
+    private $ctrlAccueil;
+    private $ctrlContact;
 
+    public function __construct() {
+        $this->ctrlAccueil = new ControleurAccueil();
+        $this->ctrlContact = new ControleurContact();
+    }
+
+    public function routerRequete() {
+        try {
+            if (isset($_GET['action'])) {
+                if ($_GET['action'] == 'contact') {
+                    $idContact = intval($this->getParametre($_GET, 'id'));
+                    if ($idContact != 0) {
+                        $this->ctrlContact->contact($idContact);
+                    }
+                    else
+                        throw new Exception("Identifiant du contact non valide");
+                }
+                else
+                    throw new Exception("Action non valide");
+            }
+            else {  // aucune action définie : affichage de l'accueil
+                $this->ctrlAccueil->accueil();
+            }
+        }
+        catch (Exception $e) {
+            $this->erreur($e->getMessage());
+        }
+    }
+
+    private function erreur($msgErreur) {
+        $vue = new View("Erreur");
+        $vue->generer(array('msgErreur' => $msgErreur));
+    }
 }
---
+++ view/vueErreur.php
@@ -0,0 +1,6 @@
+<?php $this->titre = "Erreur - Carnet"; ?>
+<div class="row">
+    <div class="col s12">
+        <p class="red-text bold center-align"><?= $msgErreur ?></p>
+    </div>
+</div>
