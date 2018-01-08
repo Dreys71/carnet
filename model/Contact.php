1974a6177dac69378cb249caf3cffa8e3c146cf8
Baptiste <baptistemoine@outlook.com>
2018-01-08 16:03:11 +0100

    Ajout du model Contact
    -> Get all contacts
    -> get contact by id

--- model/Contact.php
+++ model/Contact.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bapti
- * Date: 08/01/2018
- * Time: 14:54
- */
+require_once "Model.php";
+
+class Contact extends Modele {
+
+    public function getContacts() {
+        $sql = 'SELECT * FROM contact ORDER BY id DESC';
+        $contacts = $this->executerRequete($sql);
+        return $contacts;
+    }
+
+    public function getContact($idContact) {
+        $sql = 'SELECT * FROM contact WHERE id=?';
+        $contact = $this->executerRequete($sql, array($idContact));
+        if (!empty($contact))
+            return $contact->fetch();
+        else
+            throw new Exception("Aucun billet ne correspond à l'identifiant '$idContact'");
+    }
+}
